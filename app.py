28f438c44ac5a75e2248ad582b637a67a9a8e0e5
agustin <aalfie@itba.edu.ar>
2024-06-24 14:35:37 -0300

    changes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify, Response
 from Backend.GenAIBot import predict as bot
 import os
+from flask_cors import CORS
 
 app = Flask(__name__,  static_folder='frontend', static_url_path='')
 frontend_directory = os.path.join(os.path.dirname(__file__), 'frontend')
+CORS(app)
 
 
 @app.route('/response', methods=['POST'])
